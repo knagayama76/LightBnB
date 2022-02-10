e779d8527cd996fb07990257be1308dab12dcac7
knagayama76 <knagayama76>
2022-02-10 16:05:21 -0500

    update getAllReservations function with parameterized query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -91,11 +91,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `
+    SELECT * FROM reservations
+    JOIN properties ON property_id = properties.id
+    WHERE guest_id = $1
+    ORDER BY start_date DESC
+    LIMIT $2
+  `,
+      [guest_id, limit]
+    )
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
-
-/// Properties
 
 /**
  * Get all properties.
